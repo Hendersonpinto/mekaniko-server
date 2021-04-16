5b01b538a3b3d0f31f7b4c421c7ffaee2a3e976b
Henderson Pinto <hendersoncode@gmail.com>
2021-04-16 19:31:56 +0200

    Creates an migration for extending postgresql with postgis, creates a base for shop model and adds gitignore

--- base/models.py
+++ base/models.py
@@ -1,3 +1,5 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+class Shop(models.Model):
+    name = models.CharField(max_length=120)
+    location = models.PointField()
