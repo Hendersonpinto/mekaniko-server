6e801c9795a2d17aa6b04db50dee4fcf695f903d
Henderson Pinto <hendersoncode@gmail.com>
2021-05-01 16:36:52 +0200

    register models to admin interface

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.gis.admin import OSMGeoAdmin
-from .models import Shop
+from .models import *
 
 # Register your models here.
 
@@ -10,3 +10,13 @@ class ShopAdmin(OSMGeoAdmin):
     default_lon = 1399219
     default_lat = 7496079
     default_zoom = 12
+
+
+admin.site.register(Booking)
+admin.site.register(BookingService)
+admin.site.register(Service)
+admin.site.register(Category)
+admin.site.register(Review)
+admin.site.register(Brand)
+admin.site.register(CarModel)
+admin.site.register(Car)
